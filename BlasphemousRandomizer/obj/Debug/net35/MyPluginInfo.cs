462719f9efc005041f5db46e8dca1401567c90ec
Branden Wood <brandenek03912@hotmail.com>
2022-11-15 17:20:03 -0500

    new form

---
+++ BlasphemousRandomizer/obj/Debug/net35/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace BlasphemousRandomizer
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "BlasphemousRandomizer";
+        public const string PLUGIN_NAME = "BlasphemousRandomizer";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
