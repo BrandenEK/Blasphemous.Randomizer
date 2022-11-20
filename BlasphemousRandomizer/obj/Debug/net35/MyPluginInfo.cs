cb1d530e607a9b9adb2faf9ef1886aaeb4a22503
Branden Wood <brandenek03912@hotmail.com>
2022-11-20 14:58:51 -0500

    delete old randomizer

--- BlasphemousRandomizer/obj/Debug/net35/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BlasphemousRandomizer
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "BlasphemousRandomizer";
-        public const string PLUGIN_NAME = "BlasphemousRandomizer";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
