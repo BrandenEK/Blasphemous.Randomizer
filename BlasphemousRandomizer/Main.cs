e17b1d8837223aba5a89b73f1d7d373cf0de8e67
Branden Wood <brandenek03912@hotmail.com>
2023-03-26 18:36:27 -0400

    Fix level json files

--- BlasphemousRandomizer/Main.cs
+++ BlasphemousRandomizer/Main.cs
@@ -3,13 +3,13 @@
 namespace BlasphemousRandomizer
 {
     [BepInPlugin(MOD_ID, MOD_NAME, MOD_VERSION)]
-    [BepInDependency("com.damocles.blasphemous.modding-api", "1.3.0")]
+    [BepInDependency("com.damocles.blasphemous.modding-api", "1.3.1")]
     [BepInProcess("Blasphemous.exe")]
     public class Main : BaseUnityPlugin
     {
         public const string MOD_ID = "com.damocles.blasphemous.randomizer";
         public const string MOD_NAME = "Randomizer";
-        public const string MOD_VERSION = "1.4.4";
+        public const string MOD_VERSION = "1.4.5";
 
         public static Randomizer Randomizer;
 
