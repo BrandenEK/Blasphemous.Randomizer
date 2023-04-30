510280eb3b24191c9f5cc1a56f482f78a88cb02b
Branden Wood <brandenek03912@hotmail.com>
2023-04-30 13:40:46 -0400

    Load level edits for jondo bell room

--- BlasphemousRandomizer/Main.cs
+++ BlasphemousRandomizer/Main.cs
@@ -3,13 +3,13 @@
 namespace BlasphemousRandomizer
 {
     [BepInPlugin(MOD_ID, MOD_NAME, MOD_VERSION)]
-    [BepInDependency("com.damocles.blasphemous.modding-api", "1.3.3")]
+    [BepInDependency("com.damocles.blasphemous.modding-api", "1.3.4")]
     [BepInProcess("Blasphemous.exe")]
     public class Main : BaseUnityPlugin
     {
         public const string MOD_ID = "com.damocles.blasphemous.randomizer";
         public const string MOD_NAME = "Randomizer";
-        public const string MOD_VERSION = "9.9.9";
+        public const string MOD_VERSION = "2.0.0";
 
         public static Randomizer Randomizer;
 
