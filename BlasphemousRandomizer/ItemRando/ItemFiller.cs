5ddb895d062a1691e5854a3f41c551315c50719d
Branden Wood <brandenek03912@hotmail.com>
2023-05-26 10:19:39 -0400

    Allow null logic

--- BlasphemousRandomizer/ItemRando/ItemFiller.cs
+++ BlasphemousRandomizer/ItemRando/ItemFiller.cs
@@ -178,7 +178,7 @@ namespace BlasphemousRandomizer.ItemRando
                     DoorLocation enterDoor = newlyFoundDoors.Pop();
                     if (mappedDoors.ContainsKey(enterDoor.Id)) continue;
 
-                    if (enterDoor.Logic == null || Parser.EvaluateExpression(enterDoor.Logic, inventory))
+                    if (Parser.EvaluateExpression(enterDoor.Logic, inventory))
                     {
                         // Connect the door to vanilla/random door and add to output
                         DoorLocation exitDoor = null;
@@ -298,7 +298,7 @@ namespace BlasphemousRandomizer.ItemRando
                 for (int i = 0; i < visibleItems.Count; i++)
                 {
                     ItemLocation itemLocation = visibleItems[i];
-                    if (itemLocation.Logic == null || Parser.EvaluateExpression(itemLocation.Logic, inventory))
+                    if (Parser.EvaluateExpression(itemLocation.Logic, inventory))
                     {
                         // If long quest item and forcing junk, make it never reachable
                         if (config.JunkLongQuests && itemLocation.Type == 8)
