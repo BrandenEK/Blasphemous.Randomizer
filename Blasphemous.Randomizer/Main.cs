8f1f6eceb022dd5abec894b7dcce9a3e41a13ca1
unknown <brandenek03912@hotmail.com>
2024-05-15 15:45:40 -0400

    core: increase dependencies verison

--- Blasphemous.Randomizer/Main.cs
+++ Blasphemous.Randomizer/Main.cs
@@ -5,7 +5,8 @@ namespace Blasphemous.Randomizer
     [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
     [BepInDependency("Blasphemous.ModdingAPI", "2.1.0")]
     [BepInDependency("Blasphemous.Framework.Levels", "0.1.0")]
-    [BepInDependency("Blasphemous.Framework.Menus", "0.1.0")]
+    [BepInDependency("Blasphemous.Framework.Menus", "0.2.0")]
+    [BepInDependency("Blasphemous.Framework.UI", "0.1.1")]
     [BepInDependency("Blasphemous.CheatConsole", "1.0.0")]
     internal class Main : BaseUnityPlugin
     {
