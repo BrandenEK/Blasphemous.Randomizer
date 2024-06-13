ec390ddb6e15585c681370d6709d8d85e5577a8f
unknown <brandenek03912@hotmail.com>
2024-06-13 15:58:13 -0400

    credits: add text

--- Blasphemous.Randomizer/Services/RandomizerCredits.cs
+++ Blasphemous.Randomizer/Services/RandomizerCredits.cs
@@ -1,5 +1,6 @@
 ﻿using Blasphemous.Framework.Credits;
 using Blasphemous.Framework.Credits.Editors;
+using System.Collections.Generic;
 
 namespace Blasphemous.Randomizer.Services;
 
@@ -13,6 +14,56 @@ public class RandomizerCredits : ModCredits
     /// </summary>
     protected override void OnDisplay(ICreditsEditor editor)
     {
-        Main.Randomizer.Log("Display credits");
+        // Add logo
+
+        AddSingleSection(editor, "Programming and design", PROGRAMMING);
+        AddDoubleSection(editor, "Translations", TRANSLATIONS);
+        AddSingleSection(editor, "Images and UI", IMAGES);
+        AddSingleSection(editor, "Logic testing and improvements", LOGIC);
+
+        editor.AddDivider();
+        editor.AddBlank();
     }
+
+    private void AddSingleSection(ICreditsEditor editor, string title, string[] section)
+    {
+        editor.AddHeader(title);
+        foreach (string person in section)
+            editor.AddSingle(person);
+        editor.AddBlank();
+    }
+
+    private void AddDoubleSection(ICreditsEditor editor, string title, Dictionary<string, string> section)
+    {
+        editor.AddHeader(title);
+        foreach (var person in section)
+            editor.AddDouble(person.Key, person.Value);
+        editor.AddBlank();
+    }
+
+    private readonly string[] PROGRAMMING =
+    {
+        "Damocles"
+    };
+
+    private readonly Dictionary<string, string> TRANSLATIONS = new()
+    {
+        { "Ro", "Spanish" },
+        { "Newbie Elton", "Chinese" },
+        { "Rocher", "French" }
+    };
+
+    private readonly string[] IMAGES =
+    {
+        "Jimmy Diamonds",
+        "Raider",
+        "Ro",
+        "LuceScarlet"
+    };
+
+    private readonly string[] LOGIC =
+    {
+        "Exempt Medic",
+        "Lumineon"
+    };
 }
