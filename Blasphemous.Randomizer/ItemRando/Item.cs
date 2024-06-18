fc27ee4123a379df998255471d81a40d240190e6
unknown <brandenek03912@hotmail.com>
2024-06-18 15:11:40 -0400

    items: fix formatting and indentation

--- Blasphemous.Randomizer/ItemRando/Item.cs
+++ Blasphemous.Randomizer/ItemRando/Item.cs
@@ -4,105 +4,104 @@ using UnityEngine;
 
 namespace Blasphemous.Randomizer.ItemRando
 {
-	[System.Serializable]
     public class Item
     {
-		public string id;
-		public string name;
-		public string hint;
+        public string id;
+        public string name;
+        public string hint;
         public int type;
-		public bool progression;
-		public int count;
+        public bool progression;
+        public int count;
 
-		public int tearAmount
-		{
+        public int tearAmount
+        {
             get
             {
-				if (type != 10) return 0;
-				int s = id.IndexOf('['), e = id.IndexOf(']');
-				return int.Parse(id.Substring(s + 1, e - s - 1));
-			}
-		}
+                if (type != 10) return 0;
+                int s = id.IndexOf('['), e = id.IndexOf(']');
+                return int.Parse(id.Substring(s + 1, e - s - 1));
+            }
+        }
 
-		public bool UseDefaultImageScaling => type >= 0 && type <= 5 || type == 10 || type == 11;
+        public bool UseDefaultImageScaling => type >= 0 && type <= 5 || type == 10 || type == 11;
 
-		public Item(string id, string name, string hint, int type, bool progression, int count)
-		{
-			this.id = id;
-			this.name = name;
-			this.hint = hint;
-			this.type = type;
-			this.progression = progression;
-			this.count = count;
-		}
-
-		public virtual void addToInventory()
+        public Item(string id, string name, string hint, int type, bool progression, int count)
         {
-			InventoryManager inv = Core.InventoryManager;
-			EntityStats stats = Core.Logic.Penitent.Stats;
-			Core.Events.SetFlag("ITEM_" + id, true, false);
+            this.id = id;
+            this.name = name;
+            this.hint = hint;
+            this.type = type;
+            this.progression = progression;
+            this.count = count;
+        }
 
-			switch (type)
-			{
-				case 0:
-					inv.AddBaseObjectOrTears(inv.GetBaseObject(id, InventoryManager.ItemType.Bead));
-					if (id == "RB203" && Main.Randomizer.GameSettings.StartWithWheel)
-						inv.SetRosaryBeadInSlot(0, "RB203");
-					return;
-				case 1:
-					inv.AddBaseObjectOrTears(inv.GetBaseObject(id, InventoryManager.ItemType.Prayer)); return;
-				case 2:
-					inv.AddBaseObjectOrTears(inv.GetBaseObject(id, InventoryManager.ItemType.Relic)); return;
-				case 3:
-					inv.AddBaseObjectOrTears(inv.GetBaseObject(id, InventoryManager.ItemType.Sword)); return;
-				case 4:
-					inv.AddBaseObjectOrTears(inv.GetBaseObject(id, InventoryManager.ItemType.Collectible)); return;
-				case 5:
-					inv.AddBaseObjectOrTears(inv.GetBaseObject(id, InventoryManager.ItemType.Quest)); return;
-				case 6:
-					Core.Events.SetFlag("RESCUED_CHERUB_" + id.Substring(2), true, false); return;
-				case 7:
-					stats.Life.Upgrade(); stats.Life.SetToCurrentMax(); return;
-				case 8:
-					stats.Fervour.Upgrade(); stats.Fervour.SetToCurrentMax(); return;
-				case 9:
-					stats.Strength.Upgrade(); return;
-				case 10:
-					stats.Purge.Current += tearAmount; return;
-				case 11:
-					Core.SkillManager.UnlockSkill(id, true); return;
-				case 12:
-					return; // Do nothing, just relies on item flag
-				default:
-					return;
-			}
-		}
+        public virtual void addToInventory()
+        {
+            InventoryManager inv = Core.InventoryManager;
+            EntityStats stats = Core.Logic.Penitent.Stats;
+            Core.Events.SetFlag("ITEM_" + id, true, false);
 
-		/// <summary>
-		/// The display name of the item
-		/// </summary>
-		public virtual string GetName(bool upgraded)
-		{
+            switch (type)
+            {
+                case 0:
+                    inv.AddBaseObjectOrTears(inv.GetBaseObject(id, InventoryManager.ItemType.Bead));
+                    if (id == "RB203" && Main.Randomizer.GameSettings.StartWithWheel)
+                        inv.SetRosaryBeadInSlot(0, "RB203");
+                    return;
+                case 1:
+                    inv.AddBaseObjectOrTears(inv.GetBaseObject(id, InventoryManager.ItemType.Prayer)); return;
+                case 2:
+                    inv.AddBaseObjectOrTears(inv.GetBaseObject(id, InventoryManager.ItemType.Relic)); return;
+                case 3:
+                    inv.AddBaseObjectOrTears(inv.GetBaseObject(id, InventoryManager.ItemType.Sword)); return;
+                case 4:
+                    inv.AddBaseObjectOrTears(inv.GetBaseObject(id, InventoryManager.ItemType.Collectible)); return;
+                case 5:
+                    inv.AddBaseObjectOrTears(inv.GetBaseObject(id, InventoryManager.ItemType.Quest)); return;
+                case 6:
+                    Core.Events.SetFlag("RESCUED_CHERUB_" + id.Substring(2), true, false); return;
+                case 7:
+                    stats.Life.Upgrade(); stats.Life.SetToCurrentMax(); return;
+                case 8:
+                    stats.Fervour.Upgrade(); stats.Fervour.SetToCurrentMax(); return;
+                case 9:
+                    stats.Strength.Upgrade(); return;
+                case 10:
+                    stats.Purge.Current += tearAmount; return;
+                case 11:
+                    Core.SkillManager.UnlockSkill(id, true); return;
+                case 12:
+                    return; // Do nothing, just relies on item flag
+                default:
+                    return;
+            }
+        }
+
+        /// <summary>
+        /// The display name of the item
+        /// </summary>
+        public virtual string GetName(bool upgraded)
+        {
             InventoryManager inventory = Core.InventoryManager;
             EntityStats stats = Core.Logic.Penitent.Stats;
 
-			return type switch
-			{
-				0 => inventory.GetBaseObject(id, InventoryManager.ItemType.Bead).caption,
+            return type switch
+            {
+                0 => inventory.GetBaseObject(id, InventoryManager.ItemType.Bead).caption,
                 1 => inventory.GetBaseObject(id, InventoryManager.ItemType.Prayer).caption,
                 2 => inventory.GetBaseObject(id, InventoryManager.ItemType.Relic).caption,
                 3 => inventory.GetBaseObject(id, InventoryManager.ItemType.Sword).caption,
                 4 => inventory.GetBaseObject(id, InventoryManager.ItemType.Collectible).caption,
                 5 => inventory.GetBaseObject(id, InventoryManager.ItemType.Quest).caption,
-				6 => $"{Main.Randomizer.LocalizationHandler.Localize("chname")} {int.Parse(id.Substring(2))}/38",
+                6 => $"{Main.Randomizer.LocalizationHandler.Localize("chname")} {int.Parse(id.Substring(2))}/38",
                 7 => $"{Main.Randomizer.LocalizationHandler.Localize("luname")} {stats.Life.GetUpgrades() + (upgraded ? 1 : 0)}/6",
                 8 => $"{Main.Randomizer.LocalizationHandler.Localize("funame")} {stats.Fervour.GetUpgrades() + (upgraded ? 1 : 0)}/6",
                 9 => $"{Main.Randomizer.LocalizationHandler.Localize("suname")} {stats.Strength.GetUpgrades() + (upgraded ? 1 : 0)}/7",
                 10 => $"{Main.Randomizer.LocalizationHandler.Localize("trname")} ({tearAmount})",
-				11 => Core.SkillManager.GetSkill(id).caption.Capitalize(),
-				12 when id == "Slide" => Main.Randomizer.LocalizationHandler.Localize("dshnam"),
-				12 when id == "WallClimb" => Main.Randomizer.LocalizationHandler.Localize("wclnam"),
-				_ => "Error!"
+                11 => Core.SkillManager.GetSkill(id).caption.Capitalize(),
+                12 when id == "Slide" => Main.Randomizer.LocalizationHandler.Localize("dshnam"),
+                12 when id == "WallClimb" => Main.Randomizer.LocalizationHandler.Localize("wclnam"),
+                _ => "Error!"
             };
         }
 
@@ -110,7 +109,7 @@ namespace Blasphemous.Randomizer.ItemRando
         /// The display description of the item
         /// </summary>
         public virtual string GetDescription(bool upgraded)
-		{
+        {
             InventoryManager inventory = Core.InventoryManager;
 
             return type switch
@@ -133,14 +132,14 @@ namespace Blasphemous.Randomizer.ItemRando
             };
         }
 
-		/// <summary>
-		/// The display notification of the item
-		/// </summary>
-		public virtual string GetNotification(bool upgraded)
-		{
-			return Main.Randomizer.LocalizationHandler.Localize(type switch
-			{
-				0 => "bead",
+        /// <summary>
+        /// The display notification of the item
+        /// </summary>
+        public virtual string GetNotification(bool upgraded)
+        {
+            return Main.Randomizer.LocalizationHandler.Localize(type switch
+            {
+                0 => "bead",
                 1 => "prayer",
                 2 => "relic",
                 3 => "heart",
@@ -153,15 +152,15 @@ namespace Blasphemous.Randomizer.ItemRando
                 10 => "trnot",
                 11 => "sknot",
                 12 => "ablnot",
-				_ => "Error!"
+                _ => "Error!"
             });
-		}
+        }
 
         /// <summary>
         /// The display image of the item
         /// </summary>
         public virtual Sprite GetImage(bool upgraded)
-		{
+        {
             InventoryManager inventory = Core.InventoryManager;
 
             return type switch
@@ -183,5 +182,5 @@ namespace Blasphemous.Randomizer.ItemRando
                 _ => null
             };
         }
-	}
+    }
 }
