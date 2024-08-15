2848f51dc543e001ef02504d7519c941003dc6c0
unknown <brandenek03912@hotmail.com>
2024-08-15 18:00:54 -0400

    core: reference new libs

--- Blasphemous.Randomizer/Main.cs
+++ Blasphemous.Randomizer/Main.cs
@@ -3,12 +3,12 @@
 namespace Blasphemous.Randomizer
 {
     [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
-    [BepInDependency("Blasphemous.ModdingAPI", "2.3.0")]
-    [BepInDependency("Blasphemous.Framework.Credits", "0.1.0")]
-    [BepInDependency("Blasphemous.Framework.Levels", "0.1.0")]
-    [BepInDependency("Blasphemous.Framework.Menus", "0.3.0")]
-    [BepInDependency("Blasphemous.Framework.UI", "0.1.1")]
-    [BepInDependency("Blasphemous.CheatConsole", "1.0.0")]
+    [BepInDependency("Blasphemous.ModdingAPI", "2.4.1")]
+    [BepInDependency("Blasphemous.Framework.Credits", "0.1.1")]
+    [BepInDependency("Blasphemous.Framework.Levels", "0.1.4")]
+    [BepInDependency("Blasphemous.Framework.Menus", "0.3.4")]
+    [BepInDependency("Blasphemous.Framework.UI", "0.1.2")]
+    [BepInDependency("Blasphemous.CheatConsole", "1.0.1")]
     internal class Main : BaseUnityPlugin
     {
         public static Randomizer Randomizer { get; private set; }
