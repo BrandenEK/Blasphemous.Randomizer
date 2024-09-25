c8770bf19483d17b0be61109a780b1a6c1abad9f
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-09-25 19:50:42 -0400

    shuffle: add logic resolvers

---
+++ Blasphemous.Randomizer/ItemRando/LogicResolvers/ILogicResolver.cs
@@ -0,0 +1,29 @@
+﻿using Blasphemous.Randomizer.DoorRando;
+
+namespace Blasphemous.Randomizer.ItemRando.LogicResolvers;
+
+/// <summary>
+/// Used to determine whether a logic token is a door or item location
+/// </summary>
+public interface ILogicResolver
+{
+    /// <summary>
+    /// Checks whether the token is a door
+    /// </summary>
+    public bool IsDoor(string id);
+
+    /// <summary>
+    /// Returns the door based on the id
+    /// </summary>
+    public DoorLocation GetDoor(string id);
+
+    /// <summary>
+    /// Checks whether the token is an item locations
+    /// </summary>
+    public bool IsItemLocation(string id);
+
+    /// <summary>
+    /// Returns the item location based on the id
+    /// </summary>
+    public ItemLocation GetItemLocation(string id);
+}
---
+++ Blasphemous.Randomizer/ItemRando/LogicResolvers/RuntimeLogicResolver.cs
@@ -0,0 +1,26 @@
+﻿using Blasphemous.Randomizer.DoorRando;
+
+namespace Blasphemous.Randomizer.ItemRando.LogicResolvers;
+
+internal class RuntimeLogicResolver : ILogicResolver
+{
+    public DoorLocation GetDoor(string id)
+    {
+        return Main.Randomizer.data.doorLocations[id];
+    }
+
+    public ItemLocation GetItemLocation(string id)
+    {
+        return Main.Randomizer.data.itemLocations[id];
+    }
+
+    public bool IsDoor(string id)
+    {
+        return Main.Randomizer.data.doorLocations.ContainsKey(id);
+    }
+
+    public bool IsItemLocation(string id)
+    {
+        return Main.Randomizer.data.itemLocations.ContainsKey(id);
+    }
+}
