b19fae45f0a1a3cf2461040375017049a2791628
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-04 10:32:13 -0500

    core: upgrade dependencies

--- Blasphemous.Randomizer/Main.cs
+++ Blasphemous.Randomizer/Main.cs
@@ -4,11 +4,11 @@ namespace Blasphemous.Randomizer
 {
     [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
     [BepInDependency("Blasphemous.ModdingAPI", "3.0.0")]
-    [BepInDependency("Blasphemous.Framework.Credits", "0.1.1")]
-    [BepInDependency("Blasphemous.Framework.Levels", "0.1.4")]
-    [BepInDependency("Blasphemous.Framework.Menus", "0.4.0")]
-    [BepInDependency("Blasphemous.Framework.UI", "0.1.2")]
-    [BepInDependency("Blasphemous.CheatConsole", "1.0.1")]
+    [BepInDependency("Blasphemous.Framework.Credits", "0.2.0")]
+    [BepInDependency("Blasphemous.Framework.Levels", "0.2.0")]
+    [BepInDependency("Blasphemous.Framework.Menus", "0.6.0")]
+    [BepInDependency("Blasphemous.Framework.UI", "0.2.0")]
+    [BepInDependency("Blasphemous.CheatConsole", "1.1.0")]
     internal class Main : BaseUnityPlugin
     {
         public static Randomizer Randomizer { get; private set; }
